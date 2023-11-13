54c648b61775f0e2542740374b21e7ee4104f07c
Victor Olv <victorolvdev@gmail.com>
2023-11-13 19:43:54 -0300

    correcoes visuais da home

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,26 +1,39 @@
-import React, { useContext } from 'react'
-import styles from './Header.module.css'
-import { Link } from 'react-router-dom'
-import  Dumbbell  from '../Assets/dumbbell.svg'
-import { UserContext } from '../UserContext'
-import { FaUserCircle } from 'react-icons/fa'
+import React, { useContext } from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import Dumbbell from "../Assets/dumbbell.svg";
+import { UserContext } from "../UserContext";
+import { FaUserCircle } from "react-icons/fa";
 
 const Header = () => {
-  const {data,logado} = useContext(UserContext)
-  
+  const { data, logado } = useContext(UserContext);
+
   return (
     <header>
       <nav className={styles.navbar}>
-        <Link to='/'>
-        <img className={styles.logo} src={Dumbbell} alt="logo" height='30px'/>
-        </Link>
-          <div className={styles.links}>
-              <Link to='/'>Home</Link>
-          {logado ? <Link to='/user' className={styles.linkUser}><FaUserCircle /> {data.username} </Link> : <Link to='/login'>Login/criar</Link>}
-          </div>
+        <div className={styles.logo}>
+          <Link to="/">
+            <img
+              className={styles.logoIMG}
+              src={Dumbbell}
+              alt="logo"
+              height="30px"
+            />
+          </Link>
+        </div>
+        <div className={styles.links}>
+          <Link to="/">Home</Link>
+          {logado ? (
+            <Link to="/user" className={styles.linkUser}>
+              <FaUserCircle /> {data.username}{" "}
+            </Link>
+          ) : (
+            <Link to="/login">Login/criar</Link>
+          )}
+        </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import TyperAnimation from '../Components/TyperAnimation'
-import alexander from '../Assets/alexander.jpg'
-import styles from './Home.module.css'
-
+import React from "react";
+import TyperAnimation from "../Components/TyperAnimation";
+import alexander from "../Assets/alexander.jpg";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <section className={`${styles.homeSection} home container`}>
+    <section className={`${styles.homeSection} home`}>
       <div className={styles.homeText}>
-        <TyperAnimation texto={'Se for para sofrer que seja na academia. Lá a dor traz resultados.'} />
+        <TyperAnimation
+          texto={
+            "Se for para sofrer que seja na academia. Lá a dor traz resultados."
+          }
+        />
       </div>
       <div className={styles.homeImg}>
         <img src={alexander} alt="" />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
