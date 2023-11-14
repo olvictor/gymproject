17a279ada8cad1feb33015106e1e1e51ae5d9e18
Victor Olv <victorolvdev@gmail.com>
2023-11-14 13:38:10 -0300

    visual update

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import LoginForm from '../Components/LoginForm'
-import RegistroForm from '../Components/RegistroForm'
-import Spencer from '../Assets/spencer.jpg'
-import styles from './Login.module.css'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LoginForm from "../Components/LoginForm";
+import RegistroForm from "../Components/RegistroForm";
+import Spencer from "../Assets/spencer.jpg";
+import styles from "./Login.module.css";
 
 const Login = () => {
   return (
     <div className={`${styles.loginPage} container`}>
-      <div>
+      <div className={styles.loginBoxImg}>
         <img src={Spencer} />
       </div>
-        <Routes>
-          <Route path='/' element={<LoginForm />}></Route>
-          <Route path='registrar' element={<RegistroForm />}></Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<LoginForm />}></Route>
+        <Route path="registrar" element={<RegistroForm />}></Route>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
