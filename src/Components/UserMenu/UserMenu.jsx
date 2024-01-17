d24f01e76a8d6cb11b298aff421e3789ea09d9d4
VictorOlv <victorolvdev@gmail.com>
2024-01-17 17:43:20 -0300

    alteracoes no userMenu

--- src/Components/UserMenu/UserMenu.jsx
+++ src/Components/UserMenu/UserMenu.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import User from "../../Assets/user.svg";
-import Date from "../../Assets/date.svg";
 import Photo from "../../Assets/photo.svg";
 import Feed from "../../Assets/feed.svg";
 import Clock from "../../Assets/clock.svg";
 import Treino from "../../Assets/treino.svg";
-
+import { MdDateRange } from "react-icons/md";
 import styles from "./UserMenu.module.css";
 
 const UserMenu = () => {
@@ -19,7 +18,7 @@ const UserMenu = () => {
         <img src={Treino} />
       </NavLink>
       <NavLink to="/user/date">
-        <img src={Date} />
+        <MdDateRange />
       </NavLink>
       <NavLink to="/user/post">
         <img src={Photo} />
